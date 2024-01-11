3c8625219e8d031d07e9416d546cdee1e756f3db
Juan Castillo <51374452+jdcsalej@users.noreply.github.com>
2024-01-10 23:21:49 -0500

    Update

--- FRONT/server/main.server.mjs
+++ FRONT/server/main.server.mjs
@@ -1,2 +1,2 @@
 import './polyfills.server.mjs';
-import{a}from"./chunk-VOZXFXVN.mjs";import"./chunk-U7CVLJSS.mjs";import"./chunk-KRLCULJA.mjs";export{a as default};
+import{a}from"./chunk-LMPZVDTO.mjs";import"./chunk-U7CVLJSS.mjs";import"./chunk-KRLCULJA.mjs";export{a as default};
